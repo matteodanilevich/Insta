b5d08d1487ea7da52c27c4f9c559b832b5af55de
Mathew Danilevich <danilevichmathew@gmail.com>
2022-08-18 14:38:40 +0300

    Add initial SwiftUI Project

---
+++ Instagram_Clone/Instagram_Clone/Instagram_CloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  Instagram_CloneApp.swift
+//  Instagram_Clone
+//
+//  Created by Mateo Danilevich on 8/18/22.
+//
+
+import SwiftUI
+
+@main
+struct Instagram_CloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
