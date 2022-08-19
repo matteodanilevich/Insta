8b1e8c860b73a0ec0df20771aeddab3d8e0ffeeb
Mathew Danilevich <danilevichmathew@gmail.com>
2022-08-19 12:52:50 +0300

    Add FeedCellView with prototype of feature cell look

--- Instagram_Clone/Instagram_Clone/View/Feed/FeedView.swift
+++ Instagram_Clone/Instagram_Clone/View/Feed/FeedView.swift
@@ -9,7 +9,13 @@ import SwiftUI
 
 struct FeedView: View {
     var body: some View {
-        Text("Feed")
+        ScrollView {
+            LazyVStack(spacing: 30) {
+                ForEach(0..<10) {_ in
+                    FeedCell()
+                }
+            }
+        }
     }
 }
 
