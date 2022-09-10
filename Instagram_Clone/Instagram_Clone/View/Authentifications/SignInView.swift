671dfe536662e4543ff421a9d25cbd2ed4bcec21
Mathew Danilevich <danilevichmathew@gmail.com>
2022-09-10 19:14:55 +0300

    Added password and mail verification and made adjustments to the authorization and registration windows

--- Instagram_Clone/Instagram_Clone/View/Authentifications/SignInView.swift
+++ Instagram_Clone/Instagram_Clone/View/Authentifications/SignInView.swift
@@ -11,6 +11,8 @@ struct SignInView: View {
     
     @State var email = ""
     @State var password = ""
+    @State var emailIsValid: Bool = true
+    @State var passwordIsValid: Bool = true
     
     @EnvironmentObject var viewModel: AuthentificationViewModel
     
@@ -23,15 +25,36 @@ struct SignInView: View {
                     .scaledToFit()
                     .frame(width: 260, height: 200)
                     .foregroundColor(.black)
+                    .padding(.top, 75)
                 
                 VStack(spacing: -15) {
                     CustomTextField(text: $email, placeholder: Text("E-mail"), imageName: "envelope.circle")
                         .padding()
                         .padding(.horizontal, 32)
+                        .onChange(of: email) { newValue in
+                            if(newValue.range(of:"^\\w+([-+.']\\w+)*@\\w+([-.]\\w+)*\\.\\w+([-.]\\w+)*$", options: .regularExpression) != nil) {
+                                self.emailIsValid = true
+                                print("valid")
+                            } else {
+                                self.emailIsValid = false
+                                print("invalid")
+                            }
+                        }
+                        .foregroundColor(emailIsValid ? Color.black : Color.red)
                     
                     CustomSecureTextField(text: $password, placeholder: Text("Password"), imageName: "lock.circle")
                         .padding()
                         .padding(.horizontal, 32)
+                        .onChange(of: password) { newValue in
+                            if(newValue.range(of:"^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[$@$!%*?&#])[A-Za-z\\d$@$!%*?&#]{6,10}$", options: .regularExpression) != nil) {
+                                self.passwordIsValid = true
+                                print("valid")
+                            } else {
+                                self.passwordIsValid = false
+                                print("invalid")
+                            }
+                        }
+                        .foregroundColor(passwordIsValid ? Color.black : Color.red)
                 }
                 
                 HStack {
@@ -70,10 +93,10 @@ struct SignInView: View {
                             .font(.system(size: 14, weight: .semibold))
                         Text("Sign Up")
                             .font(.system(size: 14))
-                    }
+                    }.padding(.bottom, 10)
                 }
             }
-            .padding(.top, -110)
+            .padding(.top, -100)
         }
     }
 }
